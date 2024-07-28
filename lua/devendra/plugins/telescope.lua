72e79d86100ce1d0d872b9d902070ce4eb9a0569
Devendra Mundada <mundadad@dnb.com>
2024-07-28 21:10:20 +0530

    added new features

--- lua/devendra/plugins/telescope.lua
+++ lua/devendra/plugins/telescope.lua
@@ -14,6 +14,16 @@ return {
 		telescope.setup({
 			defaults = {
 				path_display = { "smart" },
+				vimgrep_arguments = {
+					"rg",
+					"--color=never",
+					"--no-heading",
+					"--with-filename",
+					"--line-number",
+					"--column",
+					"--smart-case",
+					"--hidden",
+				},
 				mappings = {
 					i = {
 						["<C-k>"] = actions.move_selection_previous, -- move to prev result
@@ -28,7 +38,13 @@ return {
 		-- set keymaps
 		local keymap = vim.keymap -- for conciseness
 
-		keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
+		keymap.set("n", "<leader>ff", "<cmd>Telescope find_files <cr>", { desc = "Fuzzy find files in cwd" })
+		keymap.set(
+			"n",
+			"<leader>fa",
+			"<cmd>Telescope find_files hidden=true<cr>",
+			{ desc = "Fuzzy find files in cwd with hidden files" }
+		)
 		keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
 		keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
 		keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
